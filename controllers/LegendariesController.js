8852b21bb866c8c1b09c329aa1a7d94069fa1f66
Richard Viana <richardpassostk@gmail.com>
2022-03-25 05:06:06 -0500

    Página home criada no index.ejs, e legendaries linkado no header

--- controllers/LegendariesController.js
+++ controllers/LegendariesController.js
@@ -1,9 +1,19 @@
 const legendaryModel = require('../models/LegendaryModel');
 
 const controller = {
-    getoneLegendary: (req, res) => {
+    getAllLegendary: (req, res) => {
         const model = new legendaryModel("01", "Pineco", "Bagworm", "bug")
-        return res.json(model);
+        const model2 = new legendaryModel("02", "Richard", "Bagworm", "bug");
+        const model3 = new legendaryModel("03", "Carlos", "Bagworm", "bug");
+        const model4 = new legendaryModel("04", "Matheus", "Bagworm", "bug");
+        const model5 = new legendaryModel("05", "Julia", "Bagworm", "bug");
+        return res.json([
+            model,
+            model2,
+            model3,
+            model4,
+            model5
+        ])
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const indexController = require("../controllers/indexController");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
+router.get('/', indexController.index);
 
 
 
